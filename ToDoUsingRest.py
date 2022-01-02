b40e7722bfcf221b3c29ca75d43cbbce7c66e600
Chitra <93026980+chitra-08@users.noreply.github.com>
2022-01-02 15:54:46 +0530

    Update ToDoUsingRest.py

--- ToDoUsingRest.py
+++ ToDoUsingRest.py
@@ -1,11 +1,10 @@
 import sqlite3 #import the db file
 
-conn = sqlite3.connect('ToDoUsingRest.db')#create file and connect
+conn = sqlite3.connect('ToDoUsingRest.db') #create file and connect
 
-#conn.execute('CREATE TABLE IF NOT EXISTS Items (item TEXT, status TEXT, itemDate DATETIME, itemid INTEGER PRIMARY KEY)')#create a table
-# conn.execute('CREATE TABLE IF NOT EXISTS users (uname TEXT PRIMARY KEY, pwd TEXT)')#create a table
-#print("Created Table Items")
-# res="Table created"
+conn.execute('CREATE TABLE IF NOT EXISTS Items (item TEXT, status TEXT, itemDate DATETIME, itemid INTEGER PRIMARY KEY)') #create a table
+print("Created Table Items")
+
 cursor = conn.execute("Select * from Items")
 for row in cursor:
     print(row[0]," ",row[1]," ", row[2]," ", row[3])
